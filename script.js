7830d9db74a98a1a450e22a217e71f21962e4f3a
sanjaydharsaan007@gmail.com <sanjaydharsaan007@gmail.com>
2025-07-31 11:36:15 +0530

    Changed

--- script.js
+++ script.js
@@ -23,11 +23,3 @@ for (let i = 0; i < 50; i++) {
   particle.style.animationDelay = `${Math.random() * 15}s`;
   particleContainer.appendChild(particle);
 }
-const toggleBtn = document.getElementById('theme-toggle');
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
-document.getElementById('theme-toggle').addEventListener('click', function () {
-  document.body.classList.toggle('dark-mode');
-});
-
